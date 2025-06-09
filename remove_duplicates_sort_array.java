ddac852cdba64e2ce14ee9e1d80b41f14a01b371
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:54:41 +0530

    LeetCode Q No. 26

--- remove_duplicates_sort_array.java
+++ remove_duplicates_sort_array.java
@@ -1,22 +1,24 @@
 class Solution {
 
-    // Removing duplicate function
     public int removeDuplicates(int[] nums) {
-        
+        // Edge case: If array is empty, return 0
         if (nums.length == 0) return 0;
-        
+
+        // This index will track the position of the last unique element found
         int uniqueIndex = 0;
 
-        // loop used to find duplicate index
+        // Start iterating from the second element
         for (int i = 1; i < nums.length; i++) {
-           
+            // If current number is different from the last unique one
             if (nums[i] != nums[uniqueIndex]) {
-                uniqueIndex++;
-                nums[uniqueIndex] = nums[i];
+                uniqueIndex++; // Move to the next position for a unique number
+                nums[uniqueIndex] = nums[i]; // Place the new unique number
             }
         }
-        
+
+        // Return the count of unique elements, which is index + 1
         return uniqueIndex + 1;
     }
 }
+
 
