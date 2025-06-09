6c0ae7efc17cfa9645877fc1798a697148ca4350
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 18:38:05 +0530

    LeetCode Q No . 440

---
+++ k_th_smallest_lexicographical_order.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k = k - 1;
+
+        while (k > 0) {
+            long steps = countSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                curr++;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+
+    private long countSteps(int n, long first, long last) {
+        long steps = 0;
+        while (first <= n) {
+            steps += Math.min(n + 1, last) - first;
+            first *= 10;
+            last *= 10;
+        }
+        return steps;
+    }
+}
