dfb54e88a5912f87048936f5281cc9a1526cb371
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:39:24 +0530

    LeetCode Q No. 20

--- valid_parenthisis.java
+++ valid_parenthisis.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
 
-        // Checks For the parenthisis
+        // Checks For the parenthisis () , [] and {}
   while (s.contains("()") || s.contains("[]") || s.contains("{}")) {
             s = s.replace("()", "");
             s = s.replace("[]", "");
