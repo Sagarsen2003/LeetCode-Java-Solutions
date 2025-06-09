d1a18c9621c1e7a703a4b5eed0b7083a9e4f31ec
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 12:00:15 +0530

    LeetCode Q No. 35

--- search_insert_position.java
+++ search_insert_position.java
@@ -1,25 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left =0;
-        int right = nums.length-1;
+        int left = 0;                 // Start pointer at the beginning of the array
+        int right = nums.length - 1; // End pointer at the last index
         
-    
-
-        while(left<=right){
-          int mid = left + (right - left)/2;
-       
-        if(nums[mid]==target){
-            return mid;
-        }
-        else if(nums[mid]<target){
-            left = mid+1;
-        }
-        else{
-            right = mid-1;
+        // Classic binary search loop: narrow down until left passes right
+        while (left <= right) {
+            int mid = left + (right - left) / 2;  // Calculate middle index, avoid overflow
+            
+            if (nums[mid] == target) {  // Found target, return its index immediately
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;         // Target is bigger, search right half
+            } else {
+                right = mid - 1;        // Target is smaller, search left half
+            }
         }
         
-         }
-
+        // If target not found, left will be the correct insert position
         return left;
     }
 }
+
