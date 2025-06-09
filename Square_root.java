fadb905d0b9045ef165ddaee78e5cfcd792731eb
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:47:14 +0530

    LeetCode Q No. 69

--- Square_root.java
+++ Square_root.java
@@ -1,15 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        
 
-        double output ;
+        // Declare a variable to store the square root result
+        double output;
 
+        // Use the built-in Math.sqrt() method to calculate square root of x
         output = Math.sqrt(x);
 
-
-        return  (int) output;
-
+        // Return the integer part of the square root (truncate the decimal)
+        return (int) output;
     }
-
-  
 }
+
