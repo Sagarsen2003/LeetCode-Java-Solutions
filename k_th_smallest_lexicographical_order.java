57a5a31efc049a015e18c4cb66654bfcd7f01b11
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 20:26:56 +0530

    LeetCode Q No. 440

--- k_th_smallest_lexicographical_order.java
+++ k_th_smallest_lexicographical_order.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findKthNumber(int n, int k) {
         int curr = 1;
-        k = k - 1;
+        k = k - 1;  // Because we're already at 1
 
         while (k > 0) {
             long steps = countSteps(n, curr, curr + 1);
             if (steps <= k) {
-                curr++;
-                k -= steps;
+                curr++;      // Go to next sibling
+                k -= steps;  // Skip over the whole subtree
             } else {
-                curr *= 10;
-                k--;
+                curr *= 10;  // Go to first child
+                k--;         // Count this step
             }
         }
 
