e09286576fb220953d03c66e6bc9842b14676d3d
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:49:03 +0530

    LeetCode Q No. 58

--- length_last_word.java
+++ length_last_word.java
@@ -1,9 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        // Remove any leading and trailing spaces from the string
         s = s.trim();
-        
+
+        // Find the index of the last space character in the trimmed string
         int lastSpaceIndex = s.lastIndexOf(" ");
 
+        // The length of the last word is the difference between the string's length
+        // and the index of the last space, minus 1 (to exclude the space itself)
         return s.length() - lastSpaceIndex - 1;
     }
 }
+
