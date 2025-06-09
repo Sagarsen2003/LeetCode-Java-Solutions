42fdd5a3903f8ec68790ed4afb8d1d7670253329
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:58:48 +0530

    LeetCode Q No. 13

--- roman_to_int.java
+++ roman_to_int.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 class Solution {
     public int romanToInt(String s) {
-        // Map to store Roman numeral values
+        // Step 1: Create a map to store the integer value of each Roman numeral
         HashMap<Character, Integer> romanValues = new HashMap<>();
         romanValues.put('I', 1);
         romanValues.put('V', 5);
@@ -12,20 +12,23 @@ class Solution {
         romanValues.put('D', 500);
         romanValues.put('M', 1000);
         
+        // Step 2: Initialize result to accumulate the final value
         int result = 0;  
         
-     
+        // Step 3: Traverse each character in the input string
         for (int i = 0; i < s.length(); i++) {
-            int currentVal = romanValues.get(s.charAt(i));
+            int currentVal = romanValues.get(s.charAt(i)); // Value of current Roman numeral
             
-            
+            // Step 4: Check if current value is less than the next value
+            // This handles subtractive notation like IV = 4, IX = 9, etc.
             if (i < s.length() - 1 && currentVal < romanValues.get(s.charAt(i + 1))) {
-                result -= currentVal; 
+                result -= currentVal;  // Subtract if a smaller value precedes a larger one
             } else {
-                result += currentVal; 
+                result += currentVal;  // Otherwise, add the value normally
             }
         }
         
+        // Step 5: Return the final computed integer value
         return result;  
     }
 }
