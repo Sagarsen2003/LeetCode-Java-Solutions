72125c30d403146097935bad62f701fa5c46d264
Sagar Sngupta <sagarsengupta.cse.tcea.2026@gmail.com>
2025-06-09 11:55:54 +0530

    LeetCode Q No 27

--- remove_element.java
+++ remove_element.java
@@ -1,16 +1,20 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        // 'k' will track the position to overwrite with valid (non-val) elements
         int k = 0;  
         
-        
+        // Iterate through the entire array
         for (int i = 0; i < nums.length; i++) {
-           
+            // If the current element is NOT the value to be removed
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;  
+                nums[k] = nums[i]; // Overwrite the element at index k
+                k++;  // Move to next position for valid element
             }
+            // If nums[i] == val, we just skip it
         }
         
+        // Return the count of elements not equal to 'val'
         return k;
     }
 }
+
